f23737ef297cf563f1acf6cb780c7d72380532cf
DovydasAli <dovydas.ali@gmail.com>
2021-05-03 19:52:05 +0300

    all good

--- mysite/Autoservice/admin.py
+++ mysite/Autoservice/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Service, ServiceCar, ServicePrice, ModelCar, Car, Order, OrderLine
+from .models import Service, CarService, ServicePrice, CarModel, Car, Order, OrderLine
 
 # Register your models here.
 
 admin.site.register(Service)
-admin.site.register(ServiceCar)
+admin.site.register(CarService)
 admin.site.register(ServicePrice)
-admin.site.register(ModelCar)
+admin.site.register(CarModel)
 admin.site.register(Car)
 admin.site.register(Order)
 admin.site.register(OrderLine)
--- mysite/Autoservice/models.py
+++ mysite/Autoservice/models.py
@@ -22,7 +22,7 @@ class ServicePrice(models.Model):
         return reverse('serviceprice-detail', args=[str(self.id)])
 
 
-class ModelCar(models.Model):
+class CarModel(models.Model):
     brand = models.CharField('Brand', max_length=40, help_text='Car brand')
     model = models.CharField('Model', max_length=40, help_text='Car model')
     engine = models.CharField('Engine', max_length=10, help_text='Car engine')
@@ -31,24 +31,24 @@ class ModelCar(models.Model):
         return f'{self.brand} {self.model} {self.engine}'
 
 
-class ServiceCar(models.Model):
+class CarService(models.Model):
     service_price = models.ForeignKey('ServicePrice', on_delete=models.SET_NULL, null=True)
-    car_model = models.ForeignKey('ModelCar', on_delete=models.SET_NULL, null=True)
+    car_model = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f'{self.car_model.brand} {self.car_model.model} {self.car_model.engine} - {self.service_price.name} {self.service_price.price}'
+        return f'{self.car_model.brand} {self.car_model.model} {self.car_model.engine} - {self.service_price.service.name} {self.service_price.price} eur'
 
     def get_absolute_url(self):
-        return reverse('servicecar-detail', args=[str(self.id)])
+        return reverse('carservice-detail', args=[str(self.id)])
 
 
 
 class Car(models.Model):
     client = models.CharField('Client', max_length=40, help_text='Client name')
-    car_model = models.ForeignKey('ModelCar', on_delete=models.SET_NULL, null=True)
-    license_plate = models.CharField('Client', max_length=7, help_text='3 letters and 3 numbers with a space in between'
+    car_model = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
+    license_plate = models.CharField('License plate', max_length=7, help_text='3 letters and 3 numbers with a space in between'
                                                         ' <a href="https://www.regitra.lt/lt/paslaugos/numerio-zenklai/numerio-zenklu-tipai-ir-ju-aprasymai/bendrojo-naudojimo">License plate info</a>')
-    vin_code = models.CharField('Client', max_length=17, help_text='eg. 1HGCM82633A004352 <a href="https://www.autodna.lt/vin-numeris">VIN code info</a>')
+    vin_code = models.CharField('VIN code', max_length=17, help_text='eg. 1HGCM82633A004352 <a href="https://www.autodna.lt/vin-numeris">VIN code info</a>')
 
     def __str__(self):
         return f'{self.client} {self.car_model.model} {self.license_plate} {self.vin_code}'
@@ -62,7 +62,7 @@ class Order(models.Model):
     price = models.CharField('Price', max_length=10, help_text='Price of repair')
 
     def __str__(self):
-        return f'{self.client_car.license_plate} {self.price}'
+        return f'{self.client_car.license_plate} - {self.price} eur'
 
     def get_absolute_url(self):
         return reverse('order-detail', args=[str(self.id)])
@@ -71,11 +71,11 @@ class Order(models.Model):
 class OrderLine(models.Model):
     order = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)
     service = models.ForeignKey('Service', on_delete=models.SET_NULL, null=True)
-    amount = models.CharField('Amount', max_length=10, help_text='Price of repair')
+    amount = models.CharField('Amount', max_length=10, help_text='Amount')
     price = models.CharField('Price', max_length=10, help_text='Price of repair')
 
     def __str__(self):
-        return f'{self.order.client_car.license_plate} {self.service.name} {self.amount} {self.price}'
+        return f'{self.order.client_car.license_plate} {self.service.name} - {self.amount} time(s) {self.price} eur'
 
     def get_absolute_url(self):
         return reverse('orderline-detail', args=[str(self.id)])
