02f17d4645a9586ca224ffa32009049a7063a39f
DovydasAli <dovydas.ali@gmail.com>
2021-05-03 19:02:22 +0300

    made admin and everything works

--- mysite/Autoservice/admin.py
+++ mysite/Autoservice/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Service, ServiceCar, ServicePrice, ModelCar, Car, Order, OrderLine
 
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(ServiceCar)
+admin.site.register(ServicePrice)
+admin.site.register(ModelCar)
+admin.site.register(Car)
+admin.site.register(Order)
+admin.site.register(OrderLine)
