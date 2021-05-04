ab5a43384ffb4676fd8e66c750df6967929fec06
DovydasAli <dovydas.ali@gmail.com>
2021-05-04 09:49:23 +0300

    reworked models and admin

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Service, CarService, ServicePrice, CarModel, Car, Order, OrderLine
+from .models import Service, Car, OwnerCar, Order, OrderLine
 
 # Register your models here.
 
+
 admin.site.register(Service)
-admin.site.register(CarService)
-admin.site.register(ServicePrice)
-admin.site.register(CarModel)
 admin.site.register(Car)
+admin.site.register(OwnerCar)
 admin.site.register(Order)
 admin.site.register(OrderLine)
--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,81 +1,81 @@
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Service(models.Model):
-    name = models.CharField('Name', max_length=200, help_text='Enter a service (eg. change tires)')
+    name = models.CharField('Name', max_length=200)
+    price = models.FloatField("Price")
 
     def __str__(self):
         return self.name
 
-
-class ServicePrice(models.Model):
-    service = models.ForeignKey('Service', on_delete=models.SET_NULL, null=True)
-    price = models.CharField('Price', max_length=10, help_text='Price of service')
-
-    def __str__(self):
-        return f'{self.service.name} - {self.price} eur'
-
-    def get_absolute_url(self):
-        return reverse('serviceprice-detail', args=[str(self.id)])
-
-
-class CarModel(models.Model):
-    brand = models.CharField('Brand', max_length=40, help_text='Car brand')
-    model = models.CharField('Model', max_length=40, help_text='Car model')
-    engine = models.CharField('Engine', max_length=10, help_text='Car engine')
-
-    def __str__(self):
-        return f'{self.brand} {self.model} {self.engine}'
-
-
-class CarService(models.Model):
-    service_price = models.ForeignKey('ServicePrice', on_delete=models.SET_NULL, null=True)
-    car_model = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return f'{self.car_model.brand} {self.car_model.model} {self.car_model.engine} - {self.service_price.service.name} {self.service_price.price} eur'
-
-    def get_absolute_url(self):
-        return reverse('carservice-detail', args=[str(self.id)])
-
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
 
 
 class Car(models.Model):
-    client = models.CharField('Client', max_length=40, help_text='Client name')
-    car_model = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
-    license_plate = models.CharField('License plate', max_length=7, help_text='3 letters and 3 numbers with a space in between'
-                                                        ' <a href="https://www.regitra.lt/lt/paslaugos/numerio-zenklai/numerio-zenklu-tipai-ir-ju-aprasymai/bendrojo-naudojimo">License plate info</a>')
-    vin_code = models.CharField('VIN code', max_length=17, help_text='eg. 1HGCM82633A004352 <a href="https://www.autodna.lt/vin-numeris">VIN code info</a>')
+    manufacturer = models.CharField('Manufacturer', max_length=200)
+    model = models.CharField('Model', max_length=200)
+    engine = models.CharField('Engine', max_length=200)
 
     def __str__(self):
-        return f'{self.client} {self.car_model.model} {self.license_plate} {self.vin_code}'
+        return f"{self.manufacturer} {self.model}, {self.engine}"
 
-    def get_absolute_url(self):
-        return reverse('car-detail', args=[str(self.id)])
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
+
+
+class OwnerCar(models.Model):
+    year = models.IntegerField('Year', null=True)
+    owner = models.ForeignKey(User, verbose_name="Owner", on_delete=models.SET_NULL, null=True, blank=True)
+    car = models.ForeignKey('Car', verbose_name="Model", on_delete=models.SET_NULL, null=True)
+    licence_plate = models.CharField('Licence plate', max_length=200)
+    vin_code = models.CharField('VIN code', max_length=200)
+
+    def __str__(self):
+        return f"{self.owner}: {self.car}, {self.licence_plate}, {self.vin_code}"
+
+    class Meta:
+        verbose_name = 'Owner Car'
+        verbose_name_plural = 'Owners Cars'
 
 
 class Order(models.Model):
-    client_car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)
-    price = models.CharField('Price', max_length=10, help_text='Price of repair')
+    owner_car = models.ForeignKey('OwnerCar', verbose_name="Model", on_delete=models.SET_NULL, null=True)
+    due_date = models.DateTimeField('Due Date', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.client_car.license_plate} - {self.price} eur'
+        return f"{self.owner_car}: {self.owner_car.owner}, {self.due_date}"
 
-    def get_absolute_url(self):
-        return reverse('order-detail', args=[str(self.id)])
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    STATUS = (
+        ('d', 'Draft'),
+        ('i', 'In progress'),
+        ('d', 'Done'),
+        ('c', 'Canceled'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS,
+        blank=True,
+        default='d',
+        help_text='Status',
+    )
 
 
 class OrderLine(models.Model):
-    order = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)
-    service = models.ForeignKey('Service', on_delete=models.SET_NULL, null=True)
-    amount = models.CharField('Amount', max_length=10, help_text='Amount')
-    price = models.CharField('Price', max_length=10, help_text='Price of repair')
+    order = models.ForeignKey('Order', verbose_name="Order", on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey('Service', verbose_name="Service", on_delete=models.SET_NULL, null=True)
+    qty = models.IntegerField("Quantity")
 
-    def __str__(self):
-        return f'{self.order.client_car.license_plate} {self.service.name} - {self.amount} time(s) {self.price} eur'
-
-    def get_absolute_url(self):
-        return reverse('orderline-detail', args=[str(self.id)])
+    class Meta:
+        verbose_name = 'Order Line'
+        verbose_name_plural = 'Order Lines'
